b07065f46052aa8e123b1350e16d08580be5ea02
Noah D. Brenowitz <nbren12@gmail.com>
2021-05-28 16:48:30 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Nix Packages
+------------
+
+This repo contains a package set containing 3rd party and internal dependencies
+of the climate modeling team.
---
+++ default.nix
@@ -0,0 +1,24 @@
+# Copied from https://nixos.org/nixos/nix-pills/callpackage-design-pattern.html
+let
+  fv3gfs-fortran-src = builtins.fetchGit {
+    url = "https://github.com/VulcanClimateModeling/fv3gfs-fortran.git";
+    ref = "master";
+    rev = "2d0ffc194f1b2f4974fcb5b61ab0dcef0b839854";
+  };
+  overlay = self: super: with nixpkgs; rec {
+    # ensure that everything uses mpich for mpi
+    fms = self.callPackage ./nix/fms { src=fv3gfs-fortran-src; };
+    esmf = self.callPackage ./nix/esmf { };
+    nceplibs = self.callPackage ./nix/nceplibs { };
+    fv3 = self.callPackage ./nix/fv3 { src=fv3gfs-fortran-src; };
+  };
+  nixpkgs = import (builtins.fetchTarball {
+  # Descriptive name to make the store path easier to identify
+  name = "nixos-unstable-as-of-2021-19";
+  # this commit has caches on cache.nixos.org, but doesn't update the mpich
+  # version from 20.09
+  url = "https://github.com/nixos/nixpkgs/archive/7750e6a2c95dd157d4f75a6af00923910870dd5e.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "0mw0w4mk65a6k2v6mdwa5id5rq01sjbx1klcmri9m7i77q7mzd5x";
+}) {overlays = [overlay];};
+in nixpkgs
