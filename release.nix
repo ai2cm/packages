799278b73a189cf16a90f869e7768f666bf94d92
Johann Dahm <johann.dahm@gmail.com>
2021-11-24 11:20:14 -0800

    Add serialbox to release.nix

--- release.nix
+++ release.nix
@@ -17,6 +17,7 @@ in {
   fv3gfs-wrapper = pkgs.python3Packages.fv3gfs-wrapper;
   gridtools1 = pkgs.gridtools1;
   gridtools2 = pkgs.gridtools2;
+  serialbox = pkgs.python38Packages.serialbox;
   gt4py = pkgs.python38Packages.gt4py;
   fv3core = pkgs.python38Packages.fv3core;
 }
