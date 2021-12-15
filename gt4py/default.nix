cb906b62ddcc0a00ebfbe850badf34bdca01240a
Johann Dahm <johann.dahm@gmail.com>
2021-12-15 11:58:17 -0800

    Add scipy to inputs

--- gt4py/default.nix
+++ gt4py/default.nix
@@ -1,9 +1,10 @@
 { stdenv, lib, buildPythonPackage, fetchFromGitHub, attrs, black
 , cached-property, click, dace, jinja2, numpy, packaging, pybind11, tabulate
-, typing-extensions, boltons, cytoolz, devtools, Mako, networkx, pydantic, toolz
-, typing-inspect, xxhash, boost, gridtools1, gridtools2, git, llvmPackages
-, pythonOlder, callPackage, pre-commit, isort, rope, hypothesis, pytest
-, pytest-cache, pytest-cov, pytest-factoryboy, sphinx, sphinx_rtd_theme }:
+, typing-extensions, boltons, cytoolz, scipy, devtools, Mako, networkx, pydantic
+, toolz, typing-inspect, xxhash, boost, gridtools1, gridtools2, git
+, llvmPackages, pythonOlder, callPackage, pre-commit, isort, rope, hypothesis
+, pytest, pytest-cache, pytest-cov, pytest-factoryboy, sphinx, sphinx_rtd_theme
+}:
 buildPythonPackage rec {
   pname = "gt4py";
   version = "0.1.0";
