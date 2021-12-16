b519eaa4c152bde7934a8e08261c96fa6293283e
Johann Dahm <johann.dahm@gmail.com>
2021-12-16 15:01:02 -0800

    Update GridTools

--- gridtools/1.nix
+++ gridtools/1.nix
@@ -1,12 +1,12 @@
 { stdenv, lib, fetchFromGitHub, cmake, boost, git, cacert, llvmPackages }:
 stdenv.mkDerivation rec {
   pname = "gridtools";
-  version = "1.1.3";
+  version = "1.1.4";
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "eccd3e057d6deb1c97c7b6f8233ba6bf97a96622";
-    sha256 = "167v4x0905fiqw4gcjsfxvc9wrjy4hi5xvbbimr444w9g0yy4nf7";
+    sha256 = "1mpyk85grc6zyjpjvkj29014ml5n86789068r1vj6l079yc5jw11";
   };
 
   propagatedBuildInputs = [ boost.dev ]
