2222bbb5cc2c9fd1b3cfac99108dd54b10f50ef5
Noah D. Brenowitz <nbren12@gmail.com>
2022-01-19 16:55:58 -0800

    Update to latest nixos-unstable
    this has a newer version of tensorflow

--- default.nix
+++ default.nix
@@ -30,10 +30,10 @@ let
   };
   nixpkgs = import (builtins.fetchTarball {
     # Descriptive name to make the store path easier to identify
-    name = "nixos-release-21.11";
+    name = "nix-unstable";
     url =
-      "https://github.com/nixos/nixpkgs/archive/573095944e7c1d58d30fc679c81af63668b54056.tar.gz";
+      "https://github.com/nixos/nixpkgs/archive/dbdd9f9f510cd115171d6b8048d175226fb1a62f.tar.gz";
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "07s5cwhskqvy82b4rld9b14ljc0013pig23i3jx3l3f957rk95pg";
+    sha256 = "1zcszvqfpyalyhllx4dx6chdnzaihn9kydc6aj823004xkifbpfg";
   }) { overlays = [ overlay ]; };
 in nixpkgs
